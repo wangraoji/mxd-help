8650a20e3a09b40c96220e2eef4e33b6f5fb6ca9
wangrj@vosung.cn <wangrj@vosung.cn>
2019-05-22 18:13:42 +0800

    `完成头部基础`

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,5 @@ import { MHeader } from "@/components";
 export default class App extends Vue {}
 </script>
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+
 </style>
